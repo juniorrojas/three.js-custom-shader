6ee114691fc213b1f07a84ae27c9563850274e7d
Junior Rojas <jrojasdavalos@gmail.com>
2020-08-13 19:28:51 -0600

    Reformat code

--- js/main.js
+++ js/main.js
@@ -1,29 +1,30 @@
-var width = 400;
-var height = 300;
+const width = 400;
+const height = 300;
 
-var renderer = new THREE.WebGLRenderer();
-var camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10000);
-var scene = new THREE.Scene();
+const renderer = new THREE.WebGLRenderer();
+const camera = new THREE.PerspectiveCamera(75, width / height, 0.1, 10000);
+const scene = new THREE.Scene();
+scene.background = new THREE.Color(0xbbbbbb);
 camera.position.z = 300;
 
 renderer.setSize(width, height);
-var canvas = renderer.domElement;
+const canvas = renderer.domElement;
 canvas.style.border = "1px solid black";
 document.body.appendChild(canvas);
 
-var material = new THREE.ShaderMaterial({
-	vertexShader: document.getElementById("vertexshader").text,
-	fragmentShader: document.getElementById("fragmentshader").text
+const material = new THREE.ShaderMaterial({
+  vertexShader: document.getElementById("vertexshader").text,
+  fragmentShader: document.getElementById("fragmentshader").text
 });
 
-var radius = 50;
-var widthSegments = 8;
-var heightSegments = 5;
-var geometry = new THREE.SphereGeometry(
-	radius, widthSegments, heightSegments
+const radius = 50;
+const widthSegments = 8;
+const heightSegments = 5;
+const geometry = new THREE.SphereGeometry(
+  radius, widthSegments, heightSegments
 );
 
-var sphere = new THREE.Mesh(geometry, material);
+const sphere = new THREE.Mesh(geometry, material);
 
 scene.add(sphere);
 renderer.render(scene, camera);
